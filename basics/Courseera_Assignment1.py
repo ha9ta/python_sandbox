265c83b51bddcaa719d638d5b2ef1b0e79f163fd
hassantahir <ha9ta@hotmail.com>
2019-09-02 02:12:55 -0400

    This is my first commit

---
+++ basics/Courseera_Assignment1.py
@@ -0,0 +1,8 @@
+hrs = input("Enter Hours:")
+Rate = input("Enter Hours:")
+h = float(hrs)
+R = float(Rate)
+Total1 = h * R
+Total2 = h * R
+if h > 40:
+    print(str(Total2))
---
+++ basics/first_program.py
@@ -0,0 +1,11 @@
+x = "awesomeawsome"
+
+
+def myfunc():
+    x = "fantasticfantastics"
+    print("Python is " + x)
+
+
+myfunc()
+
+print("Python is " + x)
